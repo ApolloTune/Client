ca2dfbb0e27eef5a14ff539dc8a38e6e06299381
YusufDagdeviren <ddyusufdd@gmail.com>
2023-12-14 16:32:23 +0300

    Projects skeleton created

---
+++ src/parts/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <h1 className="text-3xl font-bold text-white text-center">
+        Footer
+    </h1>
+  )
+}
+
+export default Footer
