661952c0e87cee3c46870f02b3f38b81138cfe7e
YusufDagdeviren <ddyusufdd@gmail.com>
2024-05-13 14:17:24 +0300

    api s written

---
+++ src/api/openai.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+const setAuth = () => {
+    const config = {
+        headers: {
+            Authorization: 'Bearer '+ localStorage.getItem("token")
+        }
+    }
+    return config
+}
+
+
+const keySearchWithSpotify = async(input) => {
+    const response = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/openai/keysearchrequest`, input, setAuth());
+    return response
+}
+
+const favoriteSearchWithSpotify = async() => {
+    const response = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/openai/favoritesearchrequest`, setAuth())
+    return response
+}
+const sentenceSearchWithSpotify = async(input) => {
+    const response = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/openai/sentencesearchrequest`, input, setAuth())
+    return response
+}
+const fortuneTellingByPlaylist = async(input) => {
+    const response = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/openai/sentencesearchrequest`, input, setAuth())
+    return response
+}
+export {
+    keySearchWithSpotify,
+    favoriteSearchWithSpotify,
+    sentenceSearchWithSpotify,
+    fortuneTellingByPlaylist
+}
