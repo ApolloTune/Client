dcb69c4e3323c7cb27a155551d0ddc481144eef6
YusufDagdeviren <ddyusufdd@gmail.com>
2024-05-15 15:42:27 +0300

    fortune telling finished

--- src/api/openai.js
+++ src/api/openai.js
@@ -24,7 +24,7 @@ const sentenceSearchWithSpotify = async(input) => {
     return response
 }
 const fortuneTellingByPlaylist = async(input) => {
-    const response = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/openai/sentencesearchrequest`, input, setAuth())
+    const response = await axios.post(`${process.env.REACT_APP_BASE_ENDPOINT}/openai/fortunetelling`, input, setAuth())
     return response
 }
 export {
--- src/pages/PlaylistSearch/index.jsx
+++ src/pages/PlaylistSearch/index.jsx
@@ -1,30 +1,97 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useFormik } from 'formik'
+import { fortuneTellingByPlaylist } from '../../api/openai';
+import { LinearProgress, Stack } from '@mui/material';
 
 function PlaylistSearch() {
+  const [readyData, setReadyData] = useState(true)
+  const [image, setImage] = useState("../../Images/love/love3.jpeg")
+  const [label, setLabel] = useState("aşk")
+  const [review, setReview] = useState("Aşk şarkılarını dinleme sevgin nedeniyle anladım. Kalbinin DJ'i olmalısın, çünkü romantik notaları çalma konusunda ustasın! Şu aşk şarkıları, senin duygusal playlist'inin başrol oyuncuları gibi duruyor, ne dersin?")
+  const [progress, setProgress] = useState(0)
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((oldProgress) => {
+        if (oldProgress === 100) {
+          return 0;
+        }
+        const diff = Math.random() * 10;
+        return Math.min(oldProgress + diff, 100);
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+  const { handleSubmit, handleChange, values, handleBlur } = useFormik({
+    initialValues: {
+      search: ""
+    },
+    onSubmit: async (values, bag) => {
+      try {
+        setReadyData(false)
+        let response = await fortuneTellingByPlaylist({spotifyPlayList: values.search})
+        if(response.data.emotion == "hareketli" || response.data.emotion == "enerjik"){
+          //"../../Images/love/love3.jpeg"
+          setImage("../../Images/energetic/energetic"+(Math.floor(Math.random() * 4) + 1)+".jpeg")
+        }else if(response.data.emotion == "arabesk"){
+          setImage("../../Images/arabesque/arabesque"+(Math.floor(Math.random() * 6) + 1)+".jpeg")
+        }else if (response.data.emotion == "ask" || response.data.emotion=="aşk"){
+          setImage("../../Images/love/love"+(Math.floor(Math.random() * 4) + 1)+".jpeg")
+        }else{
+          setImage("../../Images/motivation/motivation"+(Math.floor(Math.random() * 4) + 1)+".png")
+        }
+        setLabel(response.data.emotion)
+        setReview(response.data.fortune_telling)
+        setReadyData(true)
+      } catch (error) {
+        bag.setErrors({ general: error })
+      }
+    }
+  })
+
+
+
+
   return (
-    <div className='container flex justify-content flex-col min-h-[516px] bg-black-125 mt-12 border-4 border-red-50 rounded-[18px]'>
-      <input
-        className="mt-8 mx-auto placeholder:italic placeholder:text-slate-400 bg-blue-50 h-[61px] w-full md:w-[641px] border border-slate-300 rounded-[36px] py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-        placeholder="Please enter your spotify playlist link"
-        type="text"
-        name="search"
-      />
-      <p className="italic tracking-wide text-white mt-2 mx-auto">
-        Türkçe Spotify Playlist'inizin linkini girin ruh halinizin nasıl olduğunu bulalım.
-      </p>
-      <div className='mx-auto mt-6 flex flex-col items-center bg-blue-50 pt-2 pb-5 px-4 rounded-[28px] w-3/4'>
-        <img className='w-72 h-56 rounded-[30px]' src="https://raw.githubusercontent.com/ApolloTune/Client/main/Images/AskAI.jpeg" alt="Ai photo" />
-        <p className='font-sansi text-[12px] italic tracking-wider'>
-          Playlistinizin çoğunluğunu <span className="text-red-500">aşk</span> şarkıları oluşturuyor
+    <form onSubmit={handleSubmit}>
+      <div className='container flex justify-content flex-col min-h-[516px] bg-black-125 mt-12 border-4 border-red-50 rounded-[18px]'>
+        <input
+          className="mt-8 mx-auto placeholder:italic placeholder:text-slate-400 bg-blue-50 h-[61px] w-full md:w-[641px] border border-slate-300 rounded-[36px] py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+          placeholder="Please enter your spotify playlist link"
+          type="text"
+          name="search"
+          id='search'
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.search}
+        />
+        <p className="italic tracking-wide text-white mt-2 mx-auto">
+          Türkçe Spotify Playlist'inizin linkini girin ruh halinizin nasıl olduğunu bulalım.
         </p>
-        <p className='font-sansi font-black'>
-        İşte Bizim Yorumumuz
-        </p>
-        <p className='font-sansi text-[16px]'>
-        "Aşk şarkılarını dinleme sevgin nedeniyle anladım. Kalbinin DJ'i olmalısın, çünkü romantik notaları çalma konusunda ustasın! Şu aşk şarkıları, senin duygusal playlist'inin başrol oyuncuları gibi duruyor, ne dersin?"
-        </p>
+        {
+          !readyData && <div className='flex items-center justify-center flex-col'>
+            <Stack sx={{ width: '76%', color: 'grey.500', mt: 2 }} spacing={2}>
+              <LinearProgress variant='determinate' color="success" value={progress} />
+            </Stack>
+          </div>
+        }
+        <div className='mx-auto mt-6 flex flex-col items-center bg-blue-50 pt-2 pb-5 px-4 rounded-[28px] w-3/4 mb-3'>
+          <img className='w-72 h-56 rounded-[30px]' src={image} alt="Ai photo" />
+          <p className='font-sansi text-[12px] italic tracking-wider'>
+            Playlistinizin çoğunluğunu <span className="text-red-500">{label}</span> şarkıları oluşturuyor
+          </p>
+          <p className='font-sansi font-black'>
+            İşte Bizim Yorumumuz
+          </p>
+          <p className='font-sansi text-[16px]'>
+            {review}
+          </p>
+        </div>
       </div>
-    </div>
+    </form>
+
   )
 }
 
