df1e4edc12c49b3ae29eb80312b8720192f424ac
YusufDagdeviren <ddyusufdd@gmail.com>
2024-05-18 01:35:58 +0300

    favorite-songs edited and FavoriteKeyPagination added

--- src/pages/FavoriteSongs/index.jsx
+++ src/pages/FavoriteSongs/index.jsx
@@ -4,7 +4,10 @@ import { useQuery } from 'react-query'
 import KeyMusicCard from '../../components/KeyMusicCard'
 import KeyPagination from '../../parts/KeyPagination'
 import { useAuth } from '../../contexts/AuthContext'
+import { useSearchParams } from 'react-router-dom'
+import FavoriteKeyPagination from '../../parts/FavoriteKeyPagination'
 function FavoriteSongs() {
+    const [searchParams] = useSearchParams()
     const { isLoading, error, data } = useQuery(['favoritesOfUser'], () => getFavoriteSongs());
     if (isLoading) {
         return <div> Loading...
@@ -13,16 +16,30 @@ function FavoriteSongs() {
     if (error) {
         return <div>{error.message}</div>
     }
-    console.log(data); // Veri yapısını kontrol edin
 
     return (
         <div className='container flex justify-content flex-col min-h-[516px] bg-black-125 mt-12 border-4 border-red-50 rounded-[18px]'>
             <div className='grid lg:grid-cols-2 grid-cols-1 gap-2 mb-2'>
-                {data.length != 0 && data.data.map(item => {
+                {data.length != 0 && data.data.slice(
+                    searchParams.get('page') != null
+                        ? (searchParams.get('page') - 1) * 8
+                        : 0,
+                    searchParams.get('page') != null
+                        ? (searchParams.get('page') - 1) * 8 + 8
+                        : 8
+                ).map(item => {
                     return (
-                        <KeyMusicCard key={item.spotifylink} songName={item.songname} songArtist={item.songartist} songPhoto={item.songphoto} spotifyLink={item.spotifylink} />
+                        <KeyMusicCard
+                            key={item.spotifylink}
+                            songName={item.songname}
+                            songArtist={item.songartist}
+                            songPhoto={item.songphoto}
+                            spotifyLink={item.spotifylink} />
                     )
                 })}
+            </div>
+            <div className='flex justify-center items-center mb-2'>
+                <FavoriteKeyPagination data={data} />
             </div>
         </div>
     )
---
+++ src/parts/FavoriteKeyPagination/index.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import exampleData from '../../../JsonTestData/musicList'
+import { Link, useLocation, useSearchParams } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+function FavoriteKeyPagination({ data }) {
+    const [searchParams] = useSearchParams();
+    const [pageCount, setPageCount] = useState(Math.ceil(data.data.length / 8));
+    let location = useLocation();
+    const renderPageButtons = () => {
+        const buttons = [];
+
+        for (let index = 1; index <= pageCount; index++) {
+            buttons.push(
+                <button className={`text-xl ${searchParams.get('page') == index ? 'text-pink-50' : ''}`} key={index}>
+                    <Link to={`${location.pathname}?page=${index}`}>{index}</Link>
+                </button>
+            );
+        }
+
+        return buttons;
+    };
+    return (
+        <div className='container flex justify-center mt-2'>
+            <div className='flex flex-row items-center bg-white px-2 py-2 rounded-[20px] space-x-12'>
+                <button className='hover:bg-slate-100'>
+                    <Link to={`${location.pathname}?page=${searchParams.get('page') > 1 ? searchParams.get('page') - 1 : 1}`}>
+                        <FontAwesomeIcon icon={faChevronLeft} />
+                    </Link>
+                </button>
+                {renderPageButtons()}
+                <button className='hover:bg-slate-100'>
+                    <Link to={`${location.pathname}?page=${searchParams.get('page') !== null && searchParams.get('page') < pageCount ? parseInt(searchParams.get('page')) + 1 : 2}`}>
+                        <FontAwesomeIcon icon={faChevronRight} />
+                    </Link>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default FavoriteKeyPagination
